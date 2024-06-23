ce03dc23b767ced8760c4a042b86cdafefd2e1cb
Raj Yadav <rajyadav7047@gmail.com>
2024-06-24 01:05:26 +0530

    3rd Commit

---
+++ _06_LC_283_Move_Zeroes.java
@@ -0,0 +1,23 @@
+public class _06_LC_283_Move_Zeroes {
+    public static void moveZeroes(int[] nums) {
+        int j = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                j++;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int nums[] = { 0, 1, 0, 2, 3, 4 };
+        moveZeroes(nums);
+
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+}
